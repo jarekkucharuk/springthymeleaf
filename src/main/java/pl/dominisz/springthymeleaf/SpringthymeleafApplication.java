808bdaf25c61ad03b27406ed7f052206ee27047d
dominisz <dominisz@gmail.com>
2018-06-13 19:36:28 +0200

    Initial commit

---
+++ src/main/java/pl/dominisz/springthymeleaf/SpringthymeleafApplication.java
@@ -0,0 +1,12 @@
+package pl.dominisz.springthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringthymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringthymeleafApplication.class, args);
+    }
+}
